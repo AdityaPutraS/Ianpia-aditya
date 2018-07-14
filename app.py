cb13407276070b937b8c30318f4a07dd943eb8cd
AdityaPutraS <adityaputra159@gmail.com>
2018-07-14 17:17:52 +0800

    Fix app crashed

--- app.py
+++ app.py
@@ -41,9 +41,9 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 	#Simple Echo
-	isi = event.message.text
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=isi))
-		
-
+	line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text)
+        )
 if __name__ == "__main__":
     app.run()
