5296f68176621da9e99d09592dc89381985b6dec
AdityaPutraS <adityaputra159@gmail.com>
2018-07-16 03:21:16 +0800

    debug

--- app.py
+++ app.py
@@ -63,6 +63,9 @@ def handle_message(event):
                     pass
                 else:
                     os.mkdir(os.path.join(APP_ROOT,'static','test'))
+                for i in range(0,banyakPemain):
+                    pathGambar = 'static/test/'+str(i)+'.png'
+                    os.remove(pathGambar)
                 '''
                 #cek apakah dict kartu kosong
                 if(kartu):
@@ -80,11 +83,6 @@ def handle_message(event):
                     urlGambar = request.host_url+os.path.join('static','test',str(i)+'.png')
                     line_bot_api.push_message(event.source.user_id,ImageSendMessage(original_content_url = urlGambar,preview_image_url = urlGambar))
                 balas(event,'Done')
-                '''
-                for i in range(0,banyakPemain):
-                    pathGambar = 'test/'+str(i)+'.png'
-                    os.remove(pathGambar)
-                    line_bot_api.push_message(event.source.user_id,TextSendMessage(text = 'Done hapus '+str(i)))
-                '''
+
 if __name__ == "__main__":
     app.run()
