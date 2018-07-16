ad8140784e1654803b1446a3fb4e5d858ecf860b
AdityaPutraS <adityaputra159@gmail.com>
2018-07-16 11:02:23 +0800

    buat fitur berhenti dan debug

--- app.py
+++ app.py
@@ -46,26 +46,25 @@ def callback():
 def handle_postback(event):
     isiPostback = event.postback.data.split()
     nama = line_bot_api.get_profile(isiPostback[2]).display_name
-    if isiPostback[0] == 'gKB':
+    if isiPostback[0] == 'KB':
         line_bot_api.push_message(
             isiPostback[2], [
                 TextSendMessage(text='gId : '+isiPostback[1]),
                 TextSendMessage(text='uId : '+isiPostback[2])
             ]
         )
+        if(os.path.exists(os.path.join(APP_ROOT,'static',isiPostback[1]))):
+                if(os.path.exists(os.path.join(APP_ROOT,'static',isiPostback[1],isiPostback[2]))):
+                    #sudah pernah gabung
+                    line_bot_api.push_message(isiPostback[1],TemplateSendMessage(text = 'Game belum dimulai, mulai dengan ketik .kartuBohong'))
+                else:
+                    #belum pernah gabung
+                    os.mkdir(os.path.join(APP_ROOT,'static',isiPostback[1],isiPostback[2]))
+            else:
+                line_bot_api.push_message(isiPostback[1],TemplateSendMessage(text = 'Game belum dimulai, mulai dengan ketik .kartuBohong'))
         line_bot_api.push_message(
             isiPostback[1], TextSendMessage(text=nama + ' berhasil bergabung')
             )
-    elif isiPostback[0] == 'rKB':
-        line_bot_api.push_message(
-            isiPostback[2], [
-                TextSendMessage(text='rId : '+isiPostback[1]),
-                TextSendMessage(text='uId : '+isiPostback[2])
-            ]
-        )
-        line_bot_api.push_message(
-            isiPostback[1], TextSendMessage(text=nama + ' berhasil bergabung')
-            )   
 def balas(event,pesan):
     line_bot_api.reply_message(
                 event.reply_token,
@@ -110,19 +109,30 @@ def handle_message(event):
     elif(isi == '.kartuBohong'):
         uId = event.source.user_id
         valid = False
+        tipe = ''
         dataGameKartu = ''
         if(isinstance(event.source,SourceGroup)):
             gId = event.source.group_id
             valid = True
-            dataGameKartu = 'gKB '+gId+' '+uId
+            tipe = 'g'
+            dataGameKartu = 'KB '+gId+' '+uId
+            if(os.path.exists(os.path.join(APP_ROOT,'static',gId))):
+                pass
+            else:
+                os.mkdir(os.path.join(APP_ROOT,'static',gId))
         elif(isinstance(event.source,SourceRoom)):
             rId = event.source.room_id
             valid = True
-            dataGameKartu = 'rKB '+rId+' '+uId
+            tipe = 'r'
+            dataGameKartu = 'KB '+rId+' '+uId
+            if(os.path.exists(os.path.join(APP_ROOT,'static',rId))):
+                pass
+            else:
+                os.mkdir(os.path.join(APP_ROOT,'static',rId))
         else:
             balas(event,'Tidak bisa memulai permainan di 1:1 chat')
         #Kirim button ke group/room
-        if(valid):
+        if(valid):            
             buttons_template = ButtonsTemplate(
                 title='Join game Kartu Bohong', text='Klik untuk bergabung', actions=[
                     PostbackAction(label='Join', data=dataGameKartu),
@@ -133,6 +143,30 @@ def handle_message(event):
     elif(isi == '.mulai'):
         pass
     elif(isi == '.berhenti'):
-        pass
+        #check apakah game sudah ada
+        if isinstance(event.source,SourceGroup):
+            idGame = event.source.group_id
+        elif isinstance(event.source,SourceRoom):
+            idGame = event.source.room_id
+        else
+            idGame = ''
+        if(idGame == ''):
+            balas(event,'Tidak bisa digunakan di 1:1 chat')
+        else:
+            if(os.path.exists(os.path.join(APP_ROOT,'static',idGame))):
+                os.remove(os.path.join(APP_ROOT,'static',idGame))
+                balas(event,'Game berhenti')
+            else:
+                balas(event,'Game belum dimulai bahkan. Mulai dengan .kartuBohong')
+    elif(isi == 'appRoot'):
+        text = ''
+        for i in os.listdir():
+            text = text + i + ' '
+        balas(event,text)
+    elif(isi == 'static'):
+        text = ''
+        for i in os.listdir('static'):
+            text = text + i + ' '
+        balas(event,text)
 if __name__ == "__main__":
     app.run()
